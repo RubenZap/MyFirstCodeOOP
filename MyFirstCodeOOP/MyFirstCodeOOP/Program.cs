120d79c5cc7197bc0322bcab6e530d07574d3be4
RUBEN DAVID ZAPATA BRAVO <rubenzapata264702@correo.itm.edu.co>
2023-02-28 16:27:01 -0500

    Implementation in Program.cs to show the SalaryEmployee object

--- MyFirstCodeOOP/MyFirstCodeOOP/Program.cs
+++ MyFirstCodeOOP/MyFirstCodeOOP/Program.cs
@@ -10,6 +10,21 @@ namespace MyFirstCodeOOP
             {
                 var dateObject = new Date(1, 2, 3);
                 Console.WriteLine(dateObject);
+
+                Console.WriteLine("********Testing lastest implementation********");
+                //employe madre
+                //salaryEmployee hija
+                Employee salaryEmployee = new SalaryEmployee() 
+                {
+                    Id = 1000,
+                    LastName = "Posada",
+                    FirstName = "Maria", 
+                    Birthdate = new Date(1950, 2, 5),
+                    Hiringdate = new Date(2022, 12, 31),
+                    IsActive = true,
+                    Salary = 2000000.34m
+                };
+                Console.WriteLine(salaryEmployee);
             }
             catch (Exception ex) 
             {
--- MyFirstCodeOOP/MyFirstCodeOOP/SalaryEmployee.cs
+++ MyFirstCodeOOP/MyFirstCodeOOP/SalaryEmployee.cs
@@ -4,7 +4,22 @@ using System.Text;
 
 namespace MyFirstCodeOOP
 {
-    internal class SalaryEmployee
+    public class SalaryEmployee : Employee
     {
+        #region Properties
+        public decimal Salary { get; set; }
+        #endregion
+
+        #region Methods
+        public override decimal GetValueToPay()
+        {
+            return Salary;
+            
+        }
+        public override string ToString() 
+        {
+            return $"{base.ToString()} \n\t value to pay: {GetValueToPay()}"
+        }
+        #endregion
     }
 }
